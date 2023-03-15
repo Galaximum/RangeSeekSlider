7fb334de34e989e4ae73a5b630e8d5365dc4b86f
Александр Галанов <60795996+Galaximum@users.noreply.github.com>
2023-03-15 21:34:40 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,23 @@
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "RangeSeekSlider"
+    name: "RangeSeekSlider",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "RangeSeekSlider",
+            targets: ["RangeSeekSlider"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "RangeSeekSlider",
+            dependencies: [],
+            path: "RangeSeekSlider"
+        )
+    ],
+    swiftLanguageVersions: [.v5]
 )
